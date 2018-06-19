dc3cd827f97a3de34cb63d3883dcda220cc16e43
piyanooch <piyanooch.2321@gmail.com>
2018-06-19 17:12:56 +0700

    test

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -21,12 +21,23 @@
                </div>
             </div>
         
-            
+            <div id="app-4">
+              <ol>
+                <li v-for="todo in todos">
+                  @{{ todo.text }}
+                </li>
+              </ol>
+            </div>
+
+            <div id="components-demo">
+              <button-counter>fdsfsdf</button-counter>
+            </div>
        
+            
     </body>
 </html>
 <script>
-// ให้ตั้งคำถามว่าใช่ หรือ ไม่?
+
 var watchExampleVM = new Vue({
     el: '#watch-example',
     data: {
@@ -34,20 +45,14 @@ var watchExampleVM = new Vue({
       answer: 'I cannot give you an answer until you ask a question!'
     },
     watch: {
-      // whenever question changes, this function will run
+
       question: function (newQuestion, oldQuestion) {
         this.answer = 'Waiting for you to stop typing...'
         this.debouncedGetAnswer()
       }
     },
     created: function () {
-      // _.debounce is a function provided by lodash to limit how
-      // often a particularly expensive operation can be run.
-      // In this case, we want to limit how often we access
-      // yesno.wtf/api, waiting until the user has completely
-      // finished typing before making the ajax request. To learn
-      // more about the _.debounce function (and its cousin
-      // _.throttle), visit: https://lodash.com/docs#debounce
+
       this.debouncedGetAnswer = _.debounce(this.getAnswer, 500)
     },
     methods: {
@@ -75,6 +80,27 @@ var vm = new Vue({
     isActive: true,
     hasError: false
     }
-})
+});
 
-  </script>
+var app4 = new Vue({
+  el: '#app-4',
+  data: {
+    todos: [
+      { text: 'Learn JavaScript' },
+      { text: 'Learn Vue' },
+      { text: 'Build something awesome' }
+    ]
+  }
+});
+
+var n = new Vue({ 
+  el: '#components-demo',
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: '<button @click="count++">You clicked me @{{ count }} times.</button>'
+});
+
+</script>
